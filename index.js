978f0d7b3e7e86761f6b39df0911cb4555bcaaa5
Sharmaine <sharmainetaylor@ymail.com>
2022-10-11 22:47:58 +0100

    entered values for the api

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();             
 const port = 5001;                  
 
-let YOUR_WORST_FOOD = "peas"
-let YOUR_NAME = "ekemini"
+let YOUR_WORST_FOOD = "Liver"
+let YOUR_NAME = "Shar"
 
 app.get('/', (req, res) => {       
     res.send(`Welcome to ${YOUR_NAME}'s 2nd API`);     
